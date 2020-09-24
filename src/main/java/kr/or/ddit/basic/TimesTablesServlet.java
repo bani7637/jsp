a889003800b36d53274fac7d3980cc01ec08e736
bani <bban7637@gmail.com>
2020-09-24 12:04:28 +0900

    0924수업내용입니다. maven 프로젝트 설정
    basic servlet 생성
    servlet life cycle
    
    HTTP 메소드
    
    실습
    TimesTablesServlet
    
    jsp 프로젝트 git 연동 준비
    gitignore

---
+++ src/main/java/kr/or/ddit/basic/TimesTablesServlet.java
@@ -0,0 +1,40 @@
+package kr.or.ddit.basic;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TimesTablesServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html; charset=utf-8");
+
+		PrintWriter writer = resp.getWriter();
+
+		writer.println("<html>");
+		writer.println("	<head></head>");
+		writer.println("	<body>");
+		writer.println("	<table border='1'>");
+		
+		for(int i=2;i<10;i++) {
+			writer.println("	<tr>");
+		
+		for(int x=1; x<10;x++ ) {
+			writer.println("	<td>"+x+"*"+i+"="+x*i+"</td>");			
+		}
+		writer.println("	</tr>");
+		}
+		writer.println("	<table>");
+		writer.println("	</table>");
+		writer.println("	</body>");
+		writer.println("</html>");
+		writer.flush();
+		writer.close();
+	}
+}
